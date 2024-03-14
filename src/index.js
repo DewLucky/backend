6cf7bd6a6d2b1c55e3812b7200973993e79294d8
Lucky Dewangan <luckydewangan791@gmail.com>
2024-03-15 01:50:48 +0530

    Custom Error class, Custom API Response class, create user, video model, implement hashing, jwt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 // require('dotenv').config() // does not work bcz of es6 type module
 import dotenv from 'dotenv'
 import connectionDb from './db/db_connection.js';
+import {app} from './app.js';
+
 dotenv.config({path: './.env'});
 
-connectionDb();
+connectionDb()
+.then(()=>{
+    app.on("Error", (err)=>{
+        console.log("ERROR: ", err);
+        throw err;
+    })
+
+   app.listen(process.env.PORT || 8000, ()=>{
+    console.log("App is listening on port " + process.env.PORT);
+   })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!!", err)
+})
 
 
 
